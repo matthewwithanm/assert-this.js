7e0ba2acb932543fc532cd64a4d91b0547511118
Matthew Dapena-Tretter <m@tthewwithanm.com>
2015-08-18 21:13:01 -0400

    Add tests and implementation

--- src/__tests__/better-test.js
+++ src/__tests__/better-test.js
@@ -1,10 +1,63 @@
 /*eslint-env mocha */
 
 import assert from 'assert';
+import better from '../better';
 
+
+const assertIsFunction = maybeFunction => assert(typeof maybeFunction === 'function');
 
 describe('better', () => {
-  it('passes a test', () => {
-    assert(true);
+  it('can be used directly to assert', () => {
+    assert.throws(() => {
+      0::better(assert.equal, 1);
+    }, 'AssertionError: 0 == 1');
   });
+
+  describe('partial', () => {
+
+    it('returns a partially applied assertion', () => {
+      const betterEqual = better.partial(assert.equal);
+      assertIsFunction(betterEqual);
+      assert.throws(() => {
+        0::betterEqual(1);
+      }, 'AssertionError: 0 == 1');
+
+      const betterEqual1 = better.partial(assert.equal, 1);
+      assertIsFunction(betterEqual1);
+      assert.throws(() => {
+        0::betterEqual1();
+      }, 'AssertionError: 0 == 1');
+
+      const betterEqual1WithMessage = better.partial(assert.equal, 1, 'Errored!');
+      assertIsFunction(betterEqual1WithMessage);
+      assert.throws(() => {
+        0::betterEqual1WithMessage();
+      }, 'Errored!');
+    });
+
+    it('returns a function that can be further partially applied', () => {
+      const betterEqual = better.partial(assert.equal);
+
+      const betterEqual1 = betterEqual.partial(1);
+      assertIsFunction(betterEqual1);
+      assert.throws(() => {
+        0::betterEqual1();
+      }, 'AssertionError: 0 == 1');
+
+      const betterEqual1WithMessage = betterEqual.partial(1).partial('Errored!');
+      assertIsFunction(betterEqual1WithMessage);
+      assert.throws(() => {
+        0::betterEqual1WithMessage();
+      }, 'Errored!');
+    });
+
+  });
+
+  it('can be passed more args at assertion time', () => {
+    const betterEqual1 = better.partial(assert.equal, 1);
+    assert.throws(() => {
+      0::betterEqual1('Errored!');
+    }, 'Errored!');
+  });
+
 });
--- src/better.js
+++ src/better.js
@@ -1,3 +1,15 @@
-export default function better(assertion, actual, expected) {
-  
+export default function better(assertion, ...args) {
+  assertion(this, ...args);
+}
+
+better.partial = function(...args) {
+  const applied = partial(this, ...args);
+  applied.partial = better.partial;
+  return applied;
+}
+
+function partial(fn, ...args) {
+  return function(...moreArgs) {
+    return fn.call(this, ...args, ...moreArgs);
+  };
 }
